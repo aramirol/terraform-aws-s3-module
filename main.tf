4384a5b57da526f52aca72997e7e3f4bfd3be6a3
aramirol <angel.ramirolatorre@gmail.com>
2021-10-28 16:22:08 +0200

    deleting region var from main

--- main.tf
+++ main.tf
@@ -3,7 +3,6 @@
 resource "aws_s3_bucket" "bucket_test" {
   bucket = var.bucket_name
   acl    = var.bucket_acl
-  region = var.bucket_region
 
   tags = {
     Name        = "My bucket"
--- vars.tf
+++ vars.tf
@@ -9,8 +9,3 @@ variable "bucket_acl" {
   description = "ACL mode private or public"
   default = "private"
 }
-
-variable "bucket_region" {
-  description = "Region of the bucket"
-  default = "eu-central-1"
-}
